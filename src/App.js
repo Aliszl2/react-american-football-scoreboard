c1b8ed7d4829bca1305378b77897ec2bbc2ce1ad
Alison Law <alisparklaw@gmail.com>
2020-01-20 15:44:41 +0000

    Form and state hooks to update team names

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 //TODO: STEP 1 - Import the useState hook.
-import React,{ useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
 
@@ -7,12 +7,24 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
+  const [homeName, setHomeName] = useState("Lions");
+  const [awayName, setAwayName] = useState("Tigers");
+
+  const handleOnChange=event=>{}
+
+  const handleSubmitHome=event=>{
+    event.preventDefault();
+    alert(`Submitting:${homeName}`)}
+
+    const handleSubmitAway=event=>{
+      event.preventDefault();
+      alert(`Submitting:${awayName}`)}
   return (
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
+            <h2 className="home__name">{homeName}</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
@@ -20,7 +32,7 @@ function App() {
           </div>
           <div className="timer">00:03</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
+            <h2 className="away__name">{awayName}</h2>
             <div className="away__score">{awayScore}</div>
           </div>
         </div>
@@ -30,21 +42,110 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown"onClick={()=>{ setHomeScore(homeScore+7)}}>{"\u2795"}Home touchdown(+7)</button>
-  <button className="homeButtons__fieldGoal" onClick={()=>{setHomeScore(homeScore + 3)}}>{"\u2795"}Home Field Goal(+3)</button>
-        <button className="homeButtons__touchdown remove" onClick={() => setHomeScore(homeScore-7)} >{"	\u2796"}Home remove touch down(-7)</button>
-          <button className="homeButtons__fieldGoal remove" onClick={() => setHomeScore(homeScore-3)} >{"	\u2796"}Home remove field goal(-3)</button>
+
+          {/* form */}
+          <form action="submit" onSubmit={handleSubmitHome}>
+            <input type="text" name="home" placeholder="add new home team" 
+            onChange={(event)=>{setHomeName(event.target.value)}}/>
+            <button>Update Home Team</button>
+          </form>
+
+
+          <h4>Touch down</h4>
+          <div className="touch-down">
+            <button
+              className="homeButtons__touchdown"
+              onClick={() => {
+                setHomeScore(homeScore + 7);
+              }}
+            >
+              {"\u2795"}
+            </button>
+            <button
+              className="homeButtons__touchdown remove"
+              onClick={() => setHomeScore(homeScore - 7)}
+            >
+              {"	\u2796"}
+            </button>
+          </div>
+          <h4>Field goal</h4>
+          <div className="field-goal">
+            <button
+              className="homeButtons__fieldGoal"
+              onClick={() => {
+                setHomeScore(homeScore + 3);
+              }}
+            >
+              {"\u2795"}
+            </button>
+
+            <button
+              className="homeButtons__fieldGoal remove"
+              onClick={() => setHomeScore(homeScore - 3)}
+            >
+              {"	\u2796"}
+            </button>
+          </div>
         </div>
-        
-         {/* Away Buttons    */}
+
+        {/* Away Buttons    */}
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={()=>{setAwayScore(awayScore+7)}}>{"\u2795"}Away Touchdown(+7)</button>
-          <button className="awayButtons__fieldGoal"onClick={()=>{ setAwayScore(awayScore+3)}}>{"\u2795"}Away Field Goal(+3)</button>
-          <button className="awayButtons__touchdown remove" onClick={() => {setAwayScore(awayScore-7)}} >{"	\u2796"}remove away touch down</button>
-          <button className="awayButtons__fieldGoal remove" onClick={() => {setAwayScore(awayScore-3)}} >{"	\u2796"}remove away field goal</button>
+          <form action="submit"onSubmit={handleSubmitAway}>
+            <input type="text" name="away" placeholder="add new away team" 
+            onChange={(event)=>{setAwayName(event.target.value)}}
+            />
+            <button>Update Away Team</button>
+          </form>
+          <h4>Touch down</h4>
+          <div className="touch-down">
+            <button
+              className="awayButtons__touchdown"
+              onClick={() => {
+                setAwayScore(awayScore + 7);
+              }}
+            >
+              {"\u2795"}
+            </button>
+            <button
+              className="awayButtons__touchdown remove"
+              onClick={() => {
+                setAwayScore(awayScore - 7);
+              }}
+            >
+              {"	\u2796"}
+            </button>
+          </div>
+          <h4>Field goal</h4>
+          <div className="field-goal">
+            <button
+              className="awayButtons__fieldGoal"
+              onClick={() => {
+                setAwayScore(awayScore + 3);
+              }}
+            >
+              {"\u2795"}
+            </button>
+
+            <button
+              className="awayButtons__fieldGoal remove"
+              onClick={() => {
+                setAwayScore(awayScore - 3);
+              }}
+            >
+              {"	\u2796"}
+            </button>
+          </div>
         </div>
-          <button className="awayButtons__reset" onClick={() => {setAwayScore(0);setHomeScore(0)}} >Reset game</button>
       </section>
+      <button
+        className="awayButtons__reset"
+        onClick={() => {
+          setAwayScore(0);
+          setHomeScore(0);
+        }}
+      >
+        Reset game
+      </button>
     </div>
   );
 }
