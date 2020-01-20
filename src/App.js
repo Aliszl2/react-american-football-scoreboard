c2d68e81460dff29edfdd27e86ee8ac39d0ab003
Alison Law <alisparklaw@gmail.com>
2020-01-20 12:12:46 +0000

    reset and remove buttons

--- src/App.js
+++ src/App.js
@@ -26,33 +26,24 @@ function App() {
         </div>
         <BottomRow />
       </section>
+      {/* Home Buttons    */}
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown"
-          onClick={()=>{
-            setHomeScore(homeScore+7)
-
-          }}
-          >Home Touchdown(+7)</button>
-          <button className="homeButtons__fieldGoal"
-          onClick={()=>{
-            setHomeScore(homeScore + 3)
-          }}
-          >Home Field Goal(+3)</button>
+          <button className="homeButtons__touchdown"onClick={()=>{ setHomeScore(homeScore+7)}}>Home Touchdown(+7)</button>
+          <button className="homeButtons__fieldGoal" onClick={()=>{setHomeScore(homeScore + 3)}}>Home Field Goal(+3)</button>
+        <button className="homeButtons__touchdown" onClick={() => setHomeScore(homeScore-7)} >Home remove touch down(-7)</button>
+          <button className="homeButtons__fieldGoal" onClick={() => setHomeScore(homeScore-3)} >Home remove field goal(-3)</button>
         </div>
+        
+         {/* Away Buttons    */}
         <div className="awayButtons">
-          <button className="awayButtons__touchdown"
-          onClick={()=>{
-            setAwayScore(awayScore+7)
-          }}
-          >Away Touchdown(+7)</button>
-          <button className="awayButtons__fieldGoal"
-          onClick={()=>{
-            setAwayScore(awayScore+3)
-          }}
-          >Away Field Goal(+3)</button>
+          <button className="awayButtons__touchdown" onClick={()=>{setAwayScore(awayScore+7)}}>Away Touchdown(+7)</button>
+          <button className="awayButtons__fieldGoal"onClick={()=>{ setAwayScore(awayScore+3)}}>Away Field Goal(+3)</button>
+          <button className="awayButtons__touchdown" onClick={() => {setAwayScore(awayScore-7)}} >remove away touch down</button>
+          <button className="awayButtons__fieldGoal" onClick={() => {setAwayScore(awayScore-3)}} >remove away field goal</button>
         </div>
+          <button className="awayButtons__reset" onClick={() => {setAwayScore(0);setHomeScore(0)}} >Reset</button>
       </section>
     </div>
   );
